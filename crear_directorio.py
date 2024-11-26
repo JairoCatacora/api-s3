bbfac6a1b6ca034dbc41c53700e811fabe767c9f
JairoCatacora <112722355+JairoCatacora@users.noreply.github.com>
2024-11-25 19:38:48 -0500

    Update crear_directorio.py

--- crear_directorio.py
+++ crear_directorio.py
@@ -5,9 +5,10 @@ def lambda_handler(event, context):
     # Entrada
     bucket_name = event['body']['bucket_name']
     directory_name = event['body']['directory_name']
-    
+
+    reg = 'us-east-1'
     # Proceso
-    s3 = boto3.client('s3')
+    s3 = boto3.client('s3', region_name=reg)
     
     s3.put_object(Bucket=bucket_name, Key=f'{directory_name}/')
     
