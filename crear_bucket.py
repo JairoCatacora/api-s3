d32e9e7933a8554cd121f527dc623bb9c8071c34
JairoCatacora <112722355+JairoCatacora@users.noreply.github.com>
2024-11-25 19:50:56 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -5,14 +5,30 @@ def lambda_handler(event, context):
     # Entrada
     bucket_name = event['body']['bucket_name']
 
-    reg = 'us-east-1'
+    reg = 'us-east-1'  # Especifica la región aquí
     
-    # Proceso
-    s3 = boto3.client('s3',region_name=reg)
-    s3.create_bucket(Bucket=bucket_name)
+    # Inicializa el cliente de S3 con la región especificada
+    s3 = boto3.client('s3', region_name=reg)
     
-    # Salida
-    return {
-        'statusCode': 200,
-        'body': json.dumps(f'Bucket {bucket_name} creado exitosamente.')
-    }
+    try:
+        # Si la región es us-east-1, no se incluye LocationConstraint
+        if reg == 'us-east-1':
+            s3.create_bucket(Bucket=bucket_name)
+        else:
+            s3.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration={'LocationConstraint': reg}
+            )
+        
+        # Salida
+        return {
+            'statusCode': 200,
+            'body': json.dumps(f'Bucket {bucket_name} creado exitosamente en la región {reg}.')
+        }
+    
+    except Exception as e:
+        # Manejo de errores
+        return {
+            'statusCode': 400,
+            'body': json.dumps(f'Error al crear el bucket: {str(e)}')
+        }
