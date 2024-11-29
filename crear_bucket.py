258bafd78e03087603c6408a62cae994a0293ac4
JairoCatacora <alair.catacora@utec.edu.pe>
2024-11-28 23:39:41 -0500

    c1

--- crear_bucket.py
+++ crear_bucket.py
@@ -1,34 +1,26 @@
 import boto3
-import json
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
-    # Entrada
-    bucket_name = event['body']['bucket_name']
+    s3_client = boto3.client('s3')
 
-    reg = 'us-east-1'  # Especifica la región aquí
-    
-    # Inicializa el cliente de S3 con la región especificada
-    s3 = boto3.client('s3', region_name=reg)
-    
+    bucket_name = event['body'][bucket_name]
+
     try:
-        # Si la región es us-east-1, no se incluye LocationConstraint
-        if reg == 'us-east-1':
-            s3.create_bucket(Bucket=bucket_name)
-        else:
-            s3.create_bucket(
-                Bucket=bucket_name,
-                CreateBucketConfiguration={'LocationConstraint': reg}
-            )
+        response = s3_client.create_bucket(Bucket=bucket_name)
         
-        # Salida
         return {
             'statusCode': 200,
-            'body': json.dumps(f'Bucket {bucket_name} creado exitosamente en la región {reg}.')
+            'body': f'Bucket "{bucket_name}" created successfully.',
+            'response': str(response)
         }
-    
-    except Exception as e:
-        # Manejo de errores
+    except ClientError as error:
         return {
             'statusCode': 400,
-            'body': json.dumps(f'Error al crear el bucket: {str(e)}')
+            'body': f'Error creating bucket: {error.response["Error"]["Message"]}'
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Unexpected error: {str(e)}'
         }
